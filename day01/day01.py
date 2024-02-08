65e4501ebb308b2adffa056fdcc246a8345c4725
j.hwp <we1905@icloud.com>
2024-02-08 15:36:26 +0900

    v0.7

--- day01/day01.py
+++ day01/day01.py
@@ -3,71 +3,38 @@ import time
 import random
 
 ## 선형 리스트
-## 함수 선언 부분 ##
-def add_data(friend):
-    katok.append(None)
-    kLen = len(katok)
-    katok[kLen - 1] = friend
+#응용 : 다항식
+def print_poly(f_x) -> str:
+    term = len(f_x) - 1
+    poly_expression = "f(x) = "
 
-
-def insert_data(position, friend):
-    if position < 0 or position > len(katok):
-        print("데이터를 삽입할 범위를 벗어났습니다.")
-        return
-
-    katok.append(None)  # 빈칸 추가
-    kLen = len(katok)  # 배열의 현재 크기
-
-    for i in range(kLen - 1, position, -1):
-        katok[i] = katok[i - 1]
-        katok[i - 1] = None
-
-    katok[position] = friend  # 배열의 제일 뒤에 친구 추가
-
-
-def delete_data(position):
-    if position < 0 or position > len(katok):
-        print("데이터를 삭제할 범위를 벗어났습니다.")
-        return
-
-    kLen = len(katok)
-    katok[position] = None  # 데이터 삭제
-
-    for i in range(position + 1, kLen):
-        katok[i - 1] = katok[i]
-        katok[i] = None  # 배열의 제일 위치 삭제
-
-    del (katok[kLen - 1])
-
-
-## 전역 변수 선언 부분 ##
-katok = []
-select = -1  # 1: 추가, 2: 삽입, 3: 삭제, 4: 종료
-
-## 메인 코드 부분 ##
-if __name__ == "__main__":
-
-    while (select != 4):
-
-        select = int(input("선택하세요(1: 추가, 2: 삽입, 3: 삭제, 4: 종료)--> "))
-
-        if (select == 1):
-            data = input("추가할 데이터--> ")
-            add_data(data)
-            print(katok)
-        elif (select == 2):
-            pos = int(input("삽입할 위치--> "))
-            data = input("추가할 데이터--> ")
-            insert_data(pos, data)
-            print(katok)
-        elif (select == 3):
-            pos = int(input("삭제할 위치--> "))
-            delete_data(pos)
-            print(katok)
-        elif (select == 4):
-            print(katok)
-            break
-        else:
-            print("1~4 중 하나를 입력하세요.")
+    for i in range(len(fx)):
+        coefficient = f_x[i]
+        if coefficient == 0 :
+            term = term - 1
             continue
+        if coefficient > 0 & i != 0:
+            poly_expression = poly_expression + "+"
+        poly_expression = poly_expression + f'{coefficient}x^{term} '
+        term = term - 1
 
+    return poly_expression
+
+
+def calculation_poly(x_value, f_x) -> int:
+    return_value = 0
+    term = len(f_x) - 1
+
+    for i in range(len(fx)):
+        coefficient = f_x[i]
+        return_value += coefficient * pow(x_value, term)
+        term = term - 1
+
+    return return_value
+
+
+fx = [2, 3, 4, 0, -9]
+
+if __name__ == "__main__":
+    print(print_poly(fx))
+    print(calculation_poly(int(input("x 값 : ")), fx))
