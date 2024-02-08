8066d826e04f35bc85c9c314a32e471efcc193b2
j.hwp <we1905@icloud.com>
2024-02-08 13:26:06 +0900

    v0.5

--- day01/day01.py
+++ day01/day01.py
@@ -2,20 +2,17 @@ import mymath
 import time
 
 if __name__ == "__main__" : #여러 개의 파일을 실행 시 메인 파일을 확인하는 코드
-    # n = int(input("Input n : "))
-    # r = int(input("Input r : "))
+    n = int(input("Input n : "))
+    r = int(input("Input r : "))
+    print(f"{n}C{r} = {mymath.nCr(n, r)}")
+
+
     # start = time.time()
-    # print(f"{n}C{r} = {mymath.nCr(n, r)}")
+    # mymath.factorial(100)
     # end = time.time()
-    # print(f"소요시간 : {end - start}")
-
-    start = time.time()
-    mymath.factorial(100)
-    end = time.time()
-    print(f"반복문 : {end - start}")
-
-    start = time.time()
-    mymath.factorial_resursion(100)
-    end = time.time()
-    print(f"재귀 : {end - start}")
+    # print(f"반복문 : {end - start}")
+    # start = time.time()
+    # mymath.factorial_resursion(100)
+    # end = time.time()
+    # print(f"재귀 : {end - start}")
 
--- day01/mymath.py
+++ day01/mymath.py
@@ -1,3 +1,14 @@
+import time
+def timer(func) :
+    def wrapper(*args, **kwargs) :
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"소요시간 : {end - start}")
+        return result
+    return wrapper
+
+
 def factorial(number) -> int :
     result = 1
     for i in range(1, number+1) :
@@ -12,7 +23,8 @@ def factorial_resursion(number) -> int :
 
 
 
-
+#데코레이터를 이용해 시간을 출력
+@timer
 def nCr(n, r) -> int :
     """
     조합 함수
