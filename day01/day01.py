5295d0a3aae2cfdc4133c85fff5cb90782361052
j.hwp <we1905@icloud.com>
2024-02-08 14:50:21 +0900

    v0.6

--- day01/day01.py
+++ day01/day01.py
@@ -2,18 +2,72 @@ import mymath
 import time
 import random
 
-count = 7
-rand = random.randint(0,100)
-while  count != 0:
-    count -= 1
-    n = int(input("숫자를 입력하세요 : "))
-    if n == rand:
-        print("정답입니다.")
-        print(f"{7-count}번 실행했습니다.")
-        break
-    elif n > rand :
-        print(f"{n}보다 작은 수 입니다.")
-    else :
-        print(f"{n}보다 큰 수입니다.")
-else :
-    print("주어진 기회를 모두 사용했습니다.ㅜㅜ")
+## 선형 리스트
+## 함수 선언 부분 ##
+def add_data(friend):
+    katok.append(None)
+    kLen = len(katok)
+    katok[kLen - 1] = friend
+
+
+def insert_data(position, friend):
+    if position < 0 or position > len(katok):
+        print("데이터를 삽입할 범위를 벗어났습니다.")
+        return
+
+    katok.append(None)  # 빈칸 추가
+    kLen = len(katok)  # 배열의 현재 크기
+
+    for i in range(kLen - 1, position, -1):
+        katok[i] = katok[i - 1]
+        katok[i - 1] = None
+
+    katok[position] = friend  # 배열의 제일 뒤에 친구 추가
+
+
+def delete_data(position):
+    if position < 0 or position > len(katok):
+        print("데이터를 삭제할 범위를 벗어났습니다.")
+        return
+
+    kLen = len(katok)
+    katok[position] = None  # 데이터 삭제
+
+    for i in range(position + 1, kLen):
+        katok[i - 1] = katok[i]
+        katok[i] = None  # 배열의 제일 위치 삭제
+
+    del (katok[kLen - 1])
+
+
+## 전역 변수 선언 부분 ##
+katok = []
+select = -1  # 1: 추가, 2: 삽입, 3: 삭제, 4: 종료
+
+## 메인 코드 부분 ##
+if __name__ == "__main__":
+
+    while (select != 4):
+
+        select = int(input("선택하세요(1: 추가, 2: 삽입, 3: 삭제, 4: 종료)--> "))
+
+        if (select == 1):
+            data = input("추가할 데이터--> ")
+            add_data(data)
+            print(katok)
+        elif (select == 2):
+            pos = int(input("삽입할 위치--> "))
+            data = input("추가할 데이터--> ")
+            insert_data(pos, data)
+            print(katok)
+        elif (select == 3):
+            pos = int(input("삭제할 위치--> "))
+            delete_data(pos)
+            print(katok)
+        elif (select == 4):
+            print(katok)
+            break
+        else:
+            print("1~4 중 하나를 입력하세요.")
+            continue
+
