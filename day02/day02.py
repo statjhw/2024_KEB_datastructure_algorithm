16bb6019b72df859353721e739113ab04bcf3ad2
j.hwp <we1905@icloud.com>
2024-02-13 21:49:02 +0900

    v1.1

--- day02/day02.py
+++ day02/day02.py
@@ -1,36 +1,20 @@
-class Node() :
-	def __init__(self):
-		self.data = None
-		self.link = None
-
-
-def insertNode(findData, insertData) :
-	global memory, head, current, pre
-	if head.data == findData :
-		node = Node()
-		node.data = insertData
-		node.link = head
+def deleteNode(deleteData) :
+	global head, current, pre
+	if head.data == deleteData :
+		current = head
+		head = head.link
 		last = head
-		#마지막 노드 찾기
-		while last.link != head :
+		while last.link = current :
 			last = last.link
-		last.link = node
-		head = node
+		last.link = head
+		del(current)
 		return
 
 	current = head
 	while current.link != head :
-		pre = current 
+		pre = current
 		current = current.link
-		if current.data == findData :
-			node = Node()
-			node.data = insertData
-			node.link = current
-			pre.link = node
+		if current.data == deleteData :
+			pre.link = current.link
+			del(current)
 			return
-
-	node = Node()
-	node.data = insertData
- 	current.link = node
-	node.link = head
-
