16f6fdd393c18e4e1ac548ea724b1e857a35d725
j.hwp <we1905@icloud.com>
2024-02-13 23:56:12 +0900

    v1.4 queue

--- day02/day02.py
+++ day02/day02.py
@@ -1,43 +1,47 @@
-  1 def isStackFull() :
-  2     global SIZE, stack, top
-  3     if (top >= SIZE-1) :
-  4         return True
-  5     else :
-  6         return False
-  7
-  8 def isStackEmpty() :
-  9     global SIZE, stack, top
- 10     if (top == -1) :
- 11         return True
- 12     else :
- 13         return False
- 14
- 15 def push(data) :
- 16     global SIZE, stack, top
- 17     if (isStackFull()) :
- 18         print("¿¿¿ ¿ ¿¿¿¿")
- 19         return
- 20     top += 1
- 21     stack[top] = data
- 22
- 23 def pop() :
- 24     global SIZE, stack, top
- 25     if (isStackEmpty()) :
- 26         print("¿¿¿ ¿¿¿¿¿")
- 27         return None
- 28     data = stack[top]
- 29     stack[top] = None
- 30     top -= 1
- 31     return data
- 32
- 33 def peek() :
- 34     global SIZE, stack, top
- 35     if (isStackEmpty()) :
- 36         print("¿¿¿ ¿¿¿¿¿.")
- 37         return None
- 38     return stack[top]
- 39
- 40 SIZE = int(input("¿¿ ¿¿¿ ¿¿¿¿¿==>"))
- 41 stack = [None for _ in range(SIZE)]
- 42 top = -1
-~
+def isQueneFull() :
+	global SIZE, queue, front, rear
+	if rear != SIZE-1 :
+		return False
+	elif (rear == SIZE-1) and (front == -1) :
+		return True
+	else :
+		for i in range(front+1, SIZE) :
+			queue[i-1] = queue[i]
+			queue[i] = None
+		front -= 1
+		rear -= 1
+		return False
+
+def isQueueEmpty() :
+	global SIZE, queue, front, rear
+	if front == rear :
+		return True
+	else :
+		return False
+
+def enQueue(data) :
+	global SIZE, queue, front, rear
+	if (isQueueFill()) :
+		print("큐가 꽉 찼습니다.")
+		return 
+	rear += 1
+	queue[rear] = data
+
+def deQueue() :
+	global SIZE, queue, front, rear
+	if (isQueueEmpty()) :
+		print("큐가 비었습니다.")
+		return None
+	front += 1
+	data = queue[front]
+	queue[front] = None
+	return data
+
+def peek() :
+	global SIZE, queue, front, rear
+	if isQueueEmpty() :
+		print("큐가 비었습니다.")
+		return None
+	return queue[front+1]
+
+front = rear = -1
