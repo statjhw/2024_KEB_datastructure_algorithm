57dd3d55669b385d067d895eca46b25fabe69602
j.hwp <we1905@icloud.com>
2024-02-13 21:51:43 +0900

    v1.2

--- day02/day02.py
+++ day02/day02.py
@@ -1,20 +1,14 @@
-def deleteNode(deleteData) :
+def findNode(findData) :
 	global head, current, pre
-	if head.data == deleteData :
-		current = head
-		head = head.link
-		last = head
-		while last.link = current :
-			last = last.link
-		last.link = head
-		del(current)
-		return
 
 	current = head
+	if current.data == findData :
+		return current
 	while current.link != head :
-		pre = current
 		current = current.link
-		if current.data == deleteData :
-			pre.link = current.link
-			del(current)
-			return
+		if current.data == findData :
+			return current
+		
+	return Node()
+
+:
