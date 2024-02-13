3942dc4c8cc4d385cbeee6a799ac1eccf535dbf8
j.hwp <we1905@icloud.com>
2024-02-13 21:42:37 +0900

    v1.0

--- day02/day02.py
+++ day02/day02.py
@@ -1,33 +1,36 @@
-class Node :
-	def __init(self) :
+class Node() :
+	def __init__(self):
 		self.data = None
 		self.link = None
 
-def printNodes(start) :
-	current = start
-	if current.link == None :
+
+def insertNode(findData, insertData) :
+	global memory, head, current, pre
+	if head.data == findData :
+		node = Node()
+		node.data = insertData
+		node.link = head
+		last = head
+		#마지막 노드 찾기
+		while last.link != head :
+			last = last.link
+		last.link = node
+		head = node
 		return
-	print(current.data, end = ' ')
-	while current.link != start :
+
+	current = head
+	while current.link != head :
+		pre = current 
 		current = current.link
-		print(current.data, end = ' ')
-	print()
+		if current.data == findData :
+			node = Node()
+			node.data = insertData
+			node.link = current
+			pre.link = node
+			return
 
-head, current, pre = None, None, None
-dataArray = ["다현", "정연", "쯔위", "사나", "지효"]
-
-if __name__ == "__main__" :
-	
 	node = Node()
-	node.data = dataArray[0]
-	head = node
+	node.data = insertData
+ 	current.link = node
 	node.link = head
 
-	for data in dataArray[1:]:
-		pre = node
-		node = Node()
-		node.data = data
-		pre.link = node
-		node.link = head
-		
-printNodes(head)
